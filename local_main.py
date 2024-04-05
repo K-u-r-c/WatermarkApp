4785eb4822e08de493ef18b63755cd6db1057652
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2024-04-05 16:34:30 +0200

    Add .DS_Store and /build/ to .gitignore Update local_main.py to open browser on startup Update requirements.txt with new dependencies Remove django_on_heroku import from settings.py

--- local_main.py
+++ local_main.py
@@ -1,7 +1,15 @@
+import webbrowser
+from threading import Timer
 from waitress import serve
 from watermark_project.wsgi import application
 
+
+def open_browser():
+    webbrowser.open_new("http://localhost:8080/")
+
+
 if __name__ == "__main__":
+    Timer(1, open_browser).start()
     serve(application, host="0.0.0.0", port=8080)
 
-# pyinstaller --onefile --hidden-import=babel.numbers --add-data="templates:templates" local_main.py
+# pyinstaller --onefile --hidden-import=babel.numbers --hidden-import=whitenoise --add-data="templates:templates" --distpath=./build/dist --workpath=./build/work local_main.py
