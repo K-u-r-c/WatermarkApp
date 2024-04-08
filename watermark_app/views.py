c6d4860a2dea62578b776fd97c124b30f4a83d1f
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2024-04-08 14:49:46 +0200

    Update views.py

--- watermark_app/views.py
+++ watermark_app/views.py
@@ -10,7 +10,7 @@ def resize_watermark(image_size, watermark):
     return resized_watermark
 
 
-def resize_watermark(image_size, watermark):
+def rotate_watermark(watermark, angle):
     """Rotate the watermark by the given angle."""
     return watermark.rotate(angle, expand=False)
 
@@ -32,44 +32,35 @@ def apply_watermark(request):
             return HttpResponseBadRequest(
                 "Error: No image file provided or file is not an image"
             )
-
         watermark_file = request.FILES["watermark"]
         opacity = int(request.POST["opacity"])
         rotation_angle = -int(request.POST.get("rotation", 0))
-
         watermark_path = default_storage.save(
             "temp/" + watermark_file.name, watermark_file
         )
         watermark = Image.open(watermark_path).convert("RGBA")
         watermark = adjust_opacity(watermark, opacity)
         watermark = rotate_watermark(watermark, rotation_angle)
-
         image_path = default_storage.save("temp/" + image_file.name, image_file)
         image = Image.open(image_path)
-
         watermark_size_x = int(request.POST["sizeX"])
         watermark_size_y = int(request.POST["sizeY"])
         watermark_resized = resize_watermark(
             (watermark_size_x, watermark_size_y), watermark
         )
-
         x_position = int(request.POST["watermarkX"])
         y_position = int(request.POST["watermarkY"])
-
         transparent = Image.new("RGBA", (image.width, image.height), (0, 0, 0, 0))
         transparent.paste(image, (0, 0))
         transparent.paste(
             watermark_resized, (x_position, y_position), mask=watermark_resized
         )
         final_image = transparent.convert("RGB")
-
         response = HttpResponse(content_type="image/png")
         final_image.save(response, "PNG")
         response["Content-Disposition"] = 'attachment; filename="watermarked_image.png"'
-
         default_storage.delete(image_path)
         default_storage.delete(watermark_path)
-
         return response
     else:
         return render(request, "watermark_app/index.html")
